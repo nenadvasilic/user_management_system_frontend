3437831b64985e6ba1f0a577eb160f375eb0b5ff
nenadvasilic <vasilic2013@gmail.com>
2022-05-07 21:31:20 +0200

    commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Home from "./pages/Home";
+import AddEdit from "./pages/AddEdit";
+import View from "./pages/View";
+import About from "./pages/About";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <ToastContainer position="top-center" />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/add" component={AddEdit} />
+        <Route path="/update/:id" component={AddEdit} />
+        <Route path="/view/:id" component={View} />
+        <Route path="/about" component={About} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
